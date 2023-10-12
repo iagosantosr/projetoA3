7960d0294ac4f421d1a4659082dd7a2a1fa70d3c
Iago Rodrigues <iago.santos15@hotmail.com>
2023-10-11 21:19:12 -0300

    [UPDATE] atualização da tela de login do sistema, instalacao de dependencia MDB

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import '../css/home.css';
 
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+}
+
+from 'mdb-react-ui-kit';
+
 const Home = () => { 
 
-  return <div className='login-container'>
-  
-      <div className="container mt-5 d-flex justify-content-center flex-column align-items-center" id="ajustes-arealogin">
-        
-          <h1>Área de Login</h1>
-            <form>
-              <div id='usuario-area'> 
-                <label htmlFor="usuario">Usuario</label>
-                <input type="text" required name="usuario" placeholder='digite seu usuário'/>
-                </div>
-                <div id="senha-area">
-                <label htmlFor="senha">Senha</label>
-                <input type="password" required name="senha" placeholder='digite sua senha'/>
-              </div>
+  return  (
+    <MDBContainer fluid className='p-3 my-5'>
+        <MDBRow class='d-flex align-items-center'>
 
-          </form>
-      
-      </div>
-  </div>;
-};
+        <MDBCol col='10' md='6'>
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"  class="img-fluid animacao-background" alt="Phone image" style={{
+            animation: 'animacao-background 10s ease 0s 1 normal forwards'
+          }} />
+        </MDBCol>
+ 
+          <MDBCol col='4' md='6'>
+
+           <div class='mb-5 text-center'>
+            <h1>SAFE GPT</h1>
+           </div>
+
+            <MDBInput wrapperClass='mb-4' placeholder='Insira seu E-mail'  id='formControlLg'  type='email' size='lg'/>
+            <MDBInput wrapperClass='mb-4' placeholder='Insira sua Senha' id='formControlLg' type='password' size='lg'/>
+
+            <div class='d-flex justify-content-center'>
+              <MDBBtn className='mb-4 w-50 ' size='lg'>Entrar</MDBBtn>
+
+            </div>
+    
+
+          </MDBCol>
+
+        </MDBRow>
+    </MDBContainer>
+  );
+}
 
 export default Home;
